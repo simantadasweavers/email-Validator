810dc73f579925b608b394f77312615df3cf8405
Shimanta Das <shimanta.das@weavers-web.com>
2023-09-07 11:52:28 +0530

    "name module integrated"

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -1,3 +1,17 @@
+<?php
+ $host = "localhost";
+ $user = env('DB_USERNAME');
+ $dbpass = env('DB_PASSWORD');
+ $db = env('DB_DATABASE');
+ $conn = mysqli_connect($host,$user,$dbpass,$db);
+
+$mail = session()->get('visitormail');
+$sql = "SELECT name FROM visitors WHERE email = '$mail'";
+$query = mysqli_query($conn,$sql);
+$row = mysqli_fetch_assoc($query);
+$name = $row['name'];
+?>
+
 <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
             <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
               <!-- <a href="https://adminmart.com/product/modernize-free-bootstrap-admin-dashboard/" target="_blank" class="btn btn-primary">Download Free</a> -->
@@ -20,6 +34,9 @@
                       <i class="ti ti-list-check fs-6"></i>
                       <p class="mb-0 fs-3">My Task</p>
                     </a> -->
+                    <a class="d-flex align-items-center gap-2 dropdown-item">
+                      <p class="mb-0 fs-3"><?php echo $name; ?></p>
+                    </a>
                     <a href="{{url('/')}}/user_logout" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
                   </div>
                 </div>
